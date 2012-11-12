a638fc9323e76ae222b7d162e9944d3aba5da92b
Geert Weening <geertweening@gmail.com>
2012-11-12 13:27:51 +0100

    include utils in dotfiles

---
+++ .utils.sh
@@ -0,0 +1,22 @@
+grepinr() {
+    grep -inr "$1" *
+}
+
+unrar-scene() {
+    echo "unrar!"
+    SOURCE=$1
+    DESTIN=$2
+    echo "unrar from $1 to $2"
+    for f in `find $SOURCE -wholename *.r01`
+    do
+        echo "unpacking $f in directory: $2"
+        rar e -inul -o $f "$2"
+    done
+}
+
+resource() {
+  source ~/.profile
+}
+
+
+
